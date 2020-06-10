4ad676ef03cd1d2f42681ed1e8bd4cdfc2defb04
Perry Miller <perry@modelspace.com>
2020-06-10 02:47:15 -0400

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+# Generic Scene Graph
+
+An attempt to make a scene graph that is easy to understand and use, and decoupled from the rendering technology (e.g.,
+[OpenGL](https://www.khronos.org/opengl/),
+[Vulkan](https://www.khronos.org/vulkan/),
+[OSMesa](https://www.mesa3d.org/osmesa.html),
+etc).
+
+## Building
+
+You will need [CMake](https://cmake.org/) installed:
+
+    cd /your/path/to/generic_scene_graph
+    mkdir build
+    cd build
+    cmake ..
---
+++ source/GSG/Nodes/Node.h
@@ -0,0 +1,91 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2020, Perry L Miller IV
+//  All rights reserved.
+//  MIT License: https://opensource.org/licenses/mit-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Base class for all scene-graph nodes.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _GENERIC_SCENE_GRAPH_NODE_CLASS_H_
+#define _GENERIC_SCENE_GRAPH_NODE_CLASS_H_
+
+#include "GSG/Config.h"
+#include "GSG/Export.h"
+
+#include "Usul/Base/Referenced.h"
+#include "Usul/Bits/Bits.h"
+#include "Usul/Pointers/Pointers.h"
+#include "Usul/Tools/NoCopying.h"
+
+#include <atomic>
+#include <mutex>
+
+#define GSG_DECLARE_NODE_CLASS(class_name) \
+  USUL_REFERENCED_CLASS ( class_name )
+
+
+namespace GSG {
+namespace Nodes {
+
+
+class GSG_EXPORT Node : public Usul::Base::Referenced,
+  public Usul::Tools::NoCopying
+{
+public:
+
+  typedef Usul::Base::Referenced BaseClass;
+  typedef std::recursive_mutex Mutex;
+  typedef std::lock_guard < Mutex > Guard;
+  typedef std::atomic < unsigned int > Flags;
+
+  GSG_DECLARE_NODE_CLASS ( Node );
+
+  // Flags for this class.
+  enum : unsigned int
+  {
+    VISIBLE =              0x00000001,
+    INTERSECTABLE =        0x00000002,
+    CONTRIBUTE_TO_BOUNDS = 0x00000004,
+  };
+
+  // Get/set all the flags.
+  unsigned int getFlags() const { return _flags; }
+  void setFlags ( unsigned int flags ) { _flags = flags; }
+
+  // Get/set the intersectable state.
+  bool isIntersectable() const { return Usul::Bits::has < unsigned int > ( _flags, INTERSECTABLE ); }
+  void setIntersectable ( bool state ) { _flags = Usul::Bits::set < unsigned int > ( _flags, INTERSECTABLE, state ); }
+
+  // Get/set the visible state.
+  bool isVisible() const { return Usul::Bits::has < unsigned int > ( _flags, VISIBLE ); }
+  void setVisible ( bool state ) { _flags = Usul::Bits::set < unsigned int > ( _flags, VISIBLE, state ); }
+
+  // Direct access to internal mutex. Use with caution.
+  Mutex &mutex() { return _mutex; }
+
+protected:
+
+  Node();
+  virtual ~Node();
+
+private:
+
+  void _destroyNode();
+
+  mutable Mutex _mutex;
+  Flags _flags;
+};
+
+
+} // namespace Nodes
+} // namespace GSG
+
+
+#endif // _GENERIC_SCENE_GRAPH_NODE_CLASS_H_

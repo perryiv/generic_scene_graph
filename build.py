42858fb58189e56f8332924496e73746e3905c2d
Perry Miller <perry@modelspace.com>
2020-12-13 04:28:06 -0500

    Uploading boost after building it

--- build.py
+++ build.py
@@ -25,7 +25,8 @@ if __name__ == "__main__":
 
     builder = ConanMultiPackager(
         archs=["x86_64"],
-        build_policy="missing"
+        build_policy="missing",
+        upload_dependencies=["boost/1.74.0"]
     )
     builder.add_common_builds()
     for conf in builder.items:

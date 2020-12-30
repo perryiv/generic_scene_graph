a21d27a1949597db7b081b0d4d405356d4836a8d
Perry Miller <perry@modelspace.com>
2020-12-30 17:08:25 -0500

    Testing travis 17

--- build.sh
+++ build.sh
@@ -9,8 +9,50 @@
 
 echo "\n\n---- Start of build script ----\n\n"
 
+echo "----"
+env
+
+echo "----"
 cmake --version
+
+echo "----"
 $CC --version
+
+echo "----"
 $CXX --version
+
+echo "----"
+git clone https://github.com/perryiv/usul.git
+cd usul
+
+pushd .
+rm -rf build
+mkdir build
+cd build
+cmake ../ \
+  -G Ninja \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_CXX_STANDARD=${CPP_STANDARD} \
+  -DCMAKE_VERBOSE_MAKEFILE=ON \
+  -DFORCE_COLORED_OUTPUT=ON
+cmake --build .
+cd bin
+./gsg_test_d --abort --use-colour=yes --durations=no
+popd
+
+pushd .
+rm -rf build
+mkdir build
+cd build
+cmake ../../ \
+  -G Ninja \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_STANDARD=${CPP_STANDARD} \
+  -DCMAKE_VERBOSE_MAKEFILE=ON \
+  -DFORCE_COLORED_OUTPUT=ON
+cmake --build .
+cd bin
+./gsg_test --abort --use-colour=yes --durations=no
+popd
 
 echo "\n\n---- End of build script ----\n\n"

cf3b2d0b2eb72754a49addc40ad656ce33a2be1e
Perry Miller <perry@modelspace.com>
2020-12-30 16:22:07 -0500

    Testing travis 10

---
+++ build.sh
@@ -0,0 +1,11 @@
+
+################################################################################
+#
+#  Copyright (c) 2020, Perry L Miller IV
+#  All rights reserved.
+#  MIT License: https://opensource.org/licenses/mit-license.html
+#
+################################################################################
+
+gcc --version
+cmake --version

0fed5ab581f2779c99567fdda2d528c5ebb57bc5
Perry Miller <perry@modelspace.com>
2021-01-01 21:04:54 -0500

    Testing azure pipelines 40

--- scripts/before_apple.sh
+++ scripts/before_apple.sh
@@ -23,21 +23,6 @@ set -x
 echo "whoami = `whoami`"
 echo "pwd = `pwd`"
 echo "ls = `ls`"
-cmake --version
-node --version
-
-# Get ready to install.
-# sudo apt-get update
-
-# Install node.
-# curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-# sudo apt-get -y install nodejs
-
-# Make sure we own everything.
-# sudo chown -R `whoami` "/home/`whoami`/"
 
 # Install mode modules.
 npm install colors platform property-tools shelljs
-
-# Install boost.
-# sudo apt-get -y install libboost-stacktrace-dev
--- scripts/before_windows.bat
+++ scripts/before_windows.bat
@@ -17,21 +17,6 @@
 echo "whoami = `whoami`"
 echo "pwd = `pwd`"
 echo "dir = `dir`"
-cmake --version
-node --version
 
-REM :: Get ready to install.
-REM sudo apt-get update
-REM
-REM :: Install node.
-REM curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-REM sudo apt-get -y install nodejs
-REM
-REM :: Make sure we own everything.
-REM sudo chown -R `whoami` "/home/`whoami`/"
-REM
-REM :: Install mode modules.
-REM npm install colors platform property-tools shelljs
-REM
-REM :: Install boost.
-REM sudo apt-get -y install libboost-stacktrace-dev
+:: Install mode modules.
+npm install colors platform property-tools shelljs

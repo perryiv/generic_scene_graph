05d256c8fc6e3f5157760dfa567503d6bdd9124a
Perry Miller <perry@modelspace.com>
2021-01-02 03:01:53 -0500

    Testing azure pipelines 77

--- scripts/before_windows.sh
+++ scripts/before_windows.sh
@@ -20,19 +20,20 @@ set -e
 set -x
 
 # Install boost.
-# cd /tmp
-# rm -rf boost_1_75_0
-# curl -L https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.gz | tar xz
-# cd boost_1_75_0
-# ./bootstrap.sh --with-libraries=filesystem,stacktrace
-# ./b2 install
+cd /tmp
+rm -rf boost_1_75_0
+curl -L https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.gz | tar xz
+cd boost_1_75_0
+./bootstrap.sh --with-libraries=filesystem,stacktrace
+./b2 install
 
 # Install boost.
-cd /tmp
-rm -rf boost
-mkdir boost
-cd boost
-curl -L https://dl.bintray.com/boostorg/release/1.75.0/binaries/boost_1_75_0-msvc-14.2-64.exe > install_boost.exe
-./install_boost.exe
-cd /tmp
-rm -rf boost
+# This will probably be faster but it hangs when running install_boost.
+# cd /tmp
+# rm -rf boost
+# mkdir boost
+# cd boost
+# curl -L https://dl.bintray.com/boostorg/release/1.75.0/binaries/boost_1_75_0-msvc-14.2-64.exe > install_boost.exe
+# ./install_boost.exe
+# cd /tmp
+# rm -rf boost

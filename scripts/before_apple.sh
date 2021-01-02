025ec3ae111561ec9288ead064cb287a04694992
Perry Miller <perry@modelspace.com>
2021-01-02 03:07:44 -0500

    Refactored scripts

--- scripts/before_apple.sh
+++ scripts/before_apple.sh
@@ -18,3 +18,11 @@ set -e
 
 # Echo all the commands.
 set -x
+
+# Install boost.
+cd /tmp
+rm -rf boost_1_75_0
+curl -L https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.gz | tar xz
+cd boost_1_75_0
+./bootstrap.sh --with-libraries=filesystem,stacktrace
+./b2 install
--- scripts/ci.sh
+++ scripts/ci.sh
@@ -62,17 +62,6 @@ cmake --build .
 ${THIS_JOB_INSTALL_COMMAND}
 cd .. && rm -rf build
 
-# : "---- Boost ----"
-# cd /tmp
-# rm -rf boost_1_75_0
-# wget https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.bz2
-# bzip2 -d boost_1_75_0.tar.bz2
-# tar -xf boost_1_75_0.tar
-# rm boost_1_75_0.tar
-# cd boost_1_75_0
-# ./bootstrap.sh --with-libraries=filesystem,stacktrace
-# ./b2 install
-
 : "---- Usul ----"
 cd /tmp
 rm -rf usul

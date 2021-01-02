16536374762ac8232acae7ed04064e732ad91c00
Perry Miller <perry@modelspace.com>
2021-01-01 19:08:47 -0500

    Testing azure pipelines 33

--- scripts/before.sh
+++ scripts/before.sh
@@ -7,15 +7,27 @@
 #
 ################################################################################
 
+################################################################################
+#
+#  Install things needed in the continuous integration script.
+#
+################################################################################
+
 # Exit immediately if one of the following commands does not return zero.
 set -e
 
 # Echo all the commands.
 set -x
 
+# Sanity check.
+echo whoami = `whoami`
+
+# Get ready to install.
+sudo apt-get update
+
 # Install node.
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt-get -y install nodejs
 
 # Install mode modules.
-npm install -g colors platform property-tools shelljs
+sudo npm install -g colors platform property-tools shelljs

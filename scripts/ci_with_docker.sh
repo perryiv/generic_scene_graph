87a94aa1deb7846926ff6611ec764b82f383dba6
Perry Miller <perry@modelspace.com>
2021-01-03 19:16:43 -0500

    Testing azure pipelines 87

--- scripts/ci_with_docker.sh
+++ scripts/ci_with_docker.sh
@@ -13,5 +13,11 @@ set -e
 # Echo all the commands.
 set -x
 
+# Useful information.
+env | sort
+
+# Make sure these environment variables exist.
+[ -z "${THIS_JOB_DOCKER_IMAGE_NAME}"  ] && echo "Empty environment variable: THIS_JOB_DOCKER_IMAGE_NAME" && exit 1
+
 # Run the docker command.
-docker run --rm -v `pwd`:/home/conan conanio/gcc9 /bin/bash -c "sh scripts/before_ubuntu.sh && sh scripts/ci.sh"
+# docker run --rm -v `pwd`:/home/conan ${THIS_JOB_DOCKER_IMAGE_NAME} /bin/bash -c "sh scripts/before_ubuntu.sh && sh scripts/ci.sh"
